db47b3967c7cc2ad9d92cc7475bca55e3ab65b58
Parul Sura <102578176+Parul2004@users.noreply.github.com>
2023-06-30 09:09:31 -0700

    Update ArmsStrongNumberInRange.java

--- EasyQuestions/ArmsStrongNumberInRange.java
+++ EasyQuestions/ArmsStrongNumberInRange.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class ArmsStrongNumberInRange {
     public static void main(String[] args) {
+        
         Scanner ip = new Scanner(System.in);
         int a = ip.nextInt();
         int b = ip.nextInt();
